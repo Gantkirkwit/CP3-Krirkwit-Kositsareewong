f8a682ba13e1f4f33de7aa27c1f2d93cad8bfc8c
Gantkirkwit <krirkwit@gmail.com>
2023-06-23 14:44:53 +0700

    Ex5_1_Krirkwit_K.py

---
+++ Ex5_1_Krirkwit_K.py
@@ -0,0 +1,10 @@
+a = float(input("enter first number"))
+b = float(input("enter second number"))
+c = a + b
+print(a, "+", b , "=", c)
+d = a - b
+print(a, "-", b ,"=", d)
+e = a * b
+print(a, "*", b , "=", e)
+f = a / b
+print(a, "/", b , "=", f)
