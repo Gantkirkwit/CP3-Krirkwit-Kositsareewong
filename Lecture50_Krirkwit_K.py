750e2bb8a4d4846988ffed9c62221e3356705d1a
Gantkirkwit <krirkwit@gmail.com>
2023-06-27 16:17:14 +0700

    Lecture50_Krirkwit_K.py

---
+++ Lecture50_Krirkwit_K.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+addNumber(x = int(input("enter number")),y =int(input("enter number")))
+def reduction(x,y):
+    print(x-y)
+reduction(x = int(input("enter number")),y =int(input("enter number")))
+def multiply(x,y):
+    print(x*y)
+multiply(x = int(input("enter number")),y =int(input("enter number")))
+def divided(x,y):
+    print(x/y)
+divided(x = int(input("enter number")),y =int(input("enter number")))
